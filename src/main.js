6205f5e4232b7603f891bdd2702e14b5a0b3c1f2
程兰斌 <chenglanbin@huohua.cn>
2022-04-20 17:53:51 +0800

    add

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 // 引入路由
 import router from '@/router/index.bat'
+
+import { getCategoryList } from '@/api'
+getCategoryList()
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,10 +1,33 @@
 <template>
-  <div> 我是首页</div>
+  <div class="homeBox">
+    <type-nav></type-nav>
+    <list-container></list-container>
+    <recommend />
+    <rank />
+    <like />
+    <floor />
+    <brand />
+  </div>
 </template>
 
 <script>
+import TypeNav from '@/components/TypeNav'
+import ListContainer from './ListContainer'
+import Recommend from './Recommend'
+import Rank from './Rank'
+import Like from './Like'
+import Floor from './Floor'
+import Brand from './Brand'
 export default {
-
+  components:{
+    TypeNav,
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
+  }
 }
 </script>
 
